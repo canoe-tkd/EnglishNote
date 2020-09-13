b922978941d826fa13bbf9e68ce923b9c8eac6b7
takada-kazuhito <takada-kazuhito@KYCNT1906-05takadakazufumi.local>
2020-09-13 15:47:33 +0900

    データベースの基礎をやった

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'people/index'
+  get 'people', to: 'people#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'helo/index'
   get 'helo', to: 'helo#index'
