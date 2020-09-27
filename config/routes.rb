a7a58a007f8b511b81b5c959be576b922d11fc88
takada-kazuhito <takada-kazuhito@KYCNT1906-05takadakazufumi.local>
2020-09-27 17:20:27 +0900

    モデルの基礎をやった

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'people/index'
+  
+  get 'people/add'
+  post 'people/add', to: 'people#create'
+
   get 'people', to: 'people#index'
+  get 'people/:id', to: 'people#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'helo/index'
   get 'helo', to: 'helo#index'
